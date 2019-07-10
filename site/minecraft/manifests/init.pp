b0cc7f4a9c855c61f2dfaf9d82156e9cd51fec63
Carlos Rodrigues <carlos.rodrigues@unimelb.edu.au>
2019-07-10 12:30:44 +1000

    Add template to minecraft profile

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,11 @@
-class minecraft {
-    $url = 'https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar'
+class minecraft (
+    $url = 'https://launcher.mojang.com/v1/objects/d0d0fe2b1dc6ab4c65554cb734270872b72dadd6/server.jar',
     $install_dir = '/opt/minecraft'
+){
     file {$install_dir:
         ensure => directory,
     }
-    file {'${install_dir}/server.jar':
+    file {"${install_dir}/server.jar":
         ensure => file,
         source => $url,
         before => Service['minecraft'],
@@ -12,17 +13,19 @@ class minecraft {
     package {'java':
         ensure => present,
     }
-    file {'${install_dir}/eula.txt':
+    file {"${install_dir}/eula.txt":
         ensure  => file,
         content => 'eula=true',
     }
     file {'/etc/systemd/system/minecraft.service':
         ensure  => file,
-        source  => 'puppet:///modules/minecraft/minecraft.service',
+        content => epp('minecraft/minecraft.service',{
+            install_dir => $install_dir
+        }),
     }
     service {'minecraft':
         ensure => running,
         enable => true,
-        require => [Package['java'],File['${install_dir}/eula.txt'],File['/etc/systemd/system/minecraft.service']],
+        require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
     }
 }
